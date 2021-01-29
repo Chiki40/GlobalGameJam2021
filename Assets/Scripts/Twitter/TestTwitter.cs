7d033edf95a976f43a9adc5f12e37a7a4d45ef38
PC-VICTOR\Victor <victormartinezsimon@gmail.com>
2021-01-29 22:28:57 +0100

    Añado la libreria para conectarse a twitter. Añado que se puedan crear tweets, buscar tweets y subir tweets con imagenes.

---
+++ Assets/Scripts/Twitter/TestTwitter.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestTwitter : MonoBehaviour
+{
+    public Texture2D _texture;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.T))
+        {
+            TwitterManager.GetInstance().SendTweet("hola desde twitter" + Random.Range(0, 1000), callbackSendTweet);
+        }
+
+        if(Input.GetKeyDown(KeyCode.H))
+        {
+            TwitterManager.GetInstance().GetTweets(100, callbackGetTweets);
+        }
+
+        if(Input.GetKeyDown(KeyCode.I))
+        {
+            byte[] b = _texture.EncodeToPNG();
+            TwitterManager.GetInstance().SendTweetWithImage("hola con imagen " + Random.Range(0, 1000), b,callbackSendTweet);
+        }
+    }
+
+    private void callbackSendTweet(bool success, string idTweet)
+    {
+        if(success)
+        {
+            Debug.Log("he publicado el tweet bien => "+ idTweet);
+        }
+        else
+        {
+            Debug.Log("he publicado el tweet mal");
+        }
+    }
+
+    private void callbackGetTweets(bool success, List<TwitterManager.Tweet> t)
+    {
+        if(success)
+        {
+            Debug.Log("he recibido bien todos los tweets");
+
+            for(int i = 0; i < t.Count; ++i)
+            {
+                Debug.Log("ID =>" + t[i].id);
+            }
+        }
+        else
+        {
+            Debug.Log("No he recibido bien todos los tweets");
+        }
+    }
+}
