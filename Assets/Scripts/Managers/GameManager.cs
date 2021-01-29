fa4da00fd58fb266833b59c8a84bb69652b3dabe
CHIKI-PC\Daniel <chiki4089@gmail.com>
2021-01-29 18:38:51 +0100

    - UtilSound. - GameManager.

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance = null;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
