3cd7087d3cf0dcb37275bca0be48e8a9bb5637d0
CHIKI-PC\Daniel <chiki4089@gmail.com>
2021-01-30 16:10:05 +0100

    - Controller for digging action.

--- Assets/Scripts/Player/CharacterController.cs
+++ Assets/Scripts/Player/CharacterController.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 
 public class CharacterController : MonoBehaviour
 {
+    private const string kDigAnimationTag = "Dig";
     public enum EDirections { LEFT, RIGHT, BACK, FRONT, LEFT_BACK, RIGHT_BACK, LEFT_FRONT, RIGHT_FRONT };
 
     [SerializeField]
@@ -19,10 +20,12 @@ public class CharacterController : MonoBehaviour
     private PointerEventData _cachedPointerData = null;
 
     private AnimationManager _animationManager = null;
+    private Animator _animator = null;
 
     private void Awake()
     {
         _animationManager = this.GetComponent<AnimationManager>();
+        _animator = this.GetComponent<Animator>();
     }
 
 	private void OnEnable()
@@ -35,15 +38,23 @@ public class CharacterController : MonoBehaviour
     {
         EDirections direction = EDirections.LEFT;
         bool movingAnimation = false;
-        if (GetPlayerInput(out Vector3 targetDir))
+        if (GetPlayerInput(out bool dig, out Vector3 targetDir))
         {
-            float distanceToMove = _speed * Time.deltaTime;
-            Vector3 disp = targetDir.normalized * distanceToMove;
-            // Double check Y component is 0
-            disp.y = 0.0f;
-            transform.Translate(disp);
-            direction = GetDirectionFromDisp(disp.normalized);
-            movingAnimation = true;
+            if (dig)
+            {
+                _animationManager.PerformDig();
+                return;
+            }
+            else
+            {
+                float distanceToMove = _speed * Time.deltaTime;
+                Vector3 disp = targetDir.normalized * distanceToMove;
+                // Double check Y component is 0
+                disp.y = 0.0f;
+                transform.Translate(disp);
+                direction = GetDirectionFromDisp(disp.normalized);
+                movingAnimation = true;
+            }
         }
 
         if (_animationManager != null)
@@ -59,54 +70,63 @@ public class CharacterController : MonoBehaviour
         }
     }
 
-	private bool GetPlayerInput(out Vector3 destDirection)
+	private bool GetPlayerInput(out bool dig, out Vector3 destDirection)
 	{
+        dig = false;
         destDirection = Vector3.zero;
         bool input = false;
-        if (_inputEnabled && Input.GetMouseButton(0))
+
+        // Input must be enabled and character is not digging
+        if (_inputEnabled && !_animator.GetCurrentAnimatorStateInfo(0).IsTag(kDigAnimationTag))
         {
-            // Ignore if mouse is over UI
-            _cachedPointerData.position = Input.mousePosition;
-            List<RaycastResult> results = new List<RaycastResult>();
-            EventSystem.current.RaycastAll(_cachedPointerData, results);
-            if (results.Count > 0)
+            if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.E))
             {
-                return false;
+                dig = true;
+                input = true;
             }
+            else if (Input.GetMouseButton(0))
+            {
+                // Ignore if mouse is over UI
+                _cachedPointerData.position = Input.mousePosition;
+                List<RaycastResult> results = new List<RaycastResult>();
+                EventSystem.current.RaycastAll(_cachedPointerData, results);
+                if (results.Count == 0)
+                {
+                    Vector2 mouseNormalizedScreenPos = Input.mousePosition / new Vector2(Screen.width, Screen.height);
+                    Vector2 playerNormalizedScreenPos = Camera.main.WorldToScreenPoint(transform.position) / new Vector2(Screen.width, Screen.height);
+                    Vector2 dir = mouseNormalizedScreenPos - playerNormalizedScreenPos;
+                    Vector3 dir3D = new Vector3(dir.x, 0.0f, dir.y);
+                    // Skip if distance less than _moveDistanceThreshold
+                    if (dir3D.magnitude >= _moveDistanceThreshold)
+                    {
+                        destDirection = dir3D.normalized;
+                        input = true;
+                    }
+                }
+            }
+            else
+            {
+                if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+                {
+                    destDirection.z = 1.0f;
+                    input = true;
+                }
+                else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+                {
+                    destDirection.z = -1.0f;
+                    input = true;
+                }
 
-            Vector2 mouseNormalizedScreenPos = Input.mousePosition / new Vector2(Screen.width, Screen.height);
-            Vector2 playerNormalizedScreenPos = Camera.main.WorldToScreenPoint(transform.position) / new Vector2(Screen.width, Screen.height);
-            Vector2 dir = mouseNormalizedScreenPos - playerNormalizedScreenPos;
-            Vector3 dir3D = new Vector3(dir.x, 0.0f, dir.y);
-            // Skip if distance less than _moveDistanceThreshold
-            if (dir3D.magnitude >= _moveDistanceThreshold)
-            {
-                destDirection = dir3D.normalized;
-                input = true;
-            }
-        }
-        else
-		{
-            if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
-			{
-                destDirection.z = 1.0f;
-                input = true;
-            }
-            else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
-            {
-                destDirection.z = -1.0f;
-                input = true;
-            }
-
-            if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
-            {
-                destDirection.x = 1.0f;
-                input = true;
-            }
-            else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
-            {
-                destDirection.x = -1.0f;
-                input = true;
+                if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+                {
+                    destDirection.x = 1.0f;
+                    input = true;
+                }
+                else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+                {
+                    destDirection.x = -1.0f;
+                    input = true;
+                }
             }
         }
         return input;
